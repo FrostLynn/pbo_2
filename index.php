19ac96c96a38c79c41546e5296cf68e8e027abea
Akhdan Rafif Nugraha <85717156+FrostLynn@users.noreply.github.com>
2023-09-11 23:37:54 +0700

    create a class (Kendaraan)

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+class Kendaraan {
+    public $merek;
+    protected $warna;
+    private $no_plat;
 
+    public function __construct($merek, $warna, $no_plat) {
+        
+    }
+}
 
 ?>
